fcd26eacfeb5bbbd4ecdb92bb08fec7f9106c73f
uthuyein <uthuyeinlinn@gmail.com>
2025-09-02 15:46:19 +0630

    update lombok

--- 02-jdbc-api/src/main/java/com/jdc/mkt/dto/Account.java
+++ 02-jdbc-api/src/main/java/com/jdc/mkt/dto/Account.java
@@ -1,26 +1,16 @@
 package com.jdc.mkt.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class Account {
 
 	private int id;
 	private Customer customer;
 	private double balance;
 	
-	public Account(int id, Customer customer, double balance) {
-		this.id = id;
-		this.customer = customer;
-		this.balance = balance;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public Customer getCustomer() {
-		return customer;
-	}
-	public double getBalance() {
-		return balance;
-	}
 	
 	
 }
--- 02-jdbc-api/src/main/java/com/jdc/mkt/dto/Customer.java
+++ 02-jdbc-api/src/main/java/com/jdc/mkt/dto/Customer.java
@@ -1,36 +1,17 @@
 package com.jdc.mkt.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class Customer {
 
-	private int id;
-	private String name;
-	private MemberType memberType;
+	private final int id;
+	private final String name;
+	private final MemberType memberType;
 	private boolean active;
 		
-	public Customer(int id, String name,MemberType memberType) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.memberType = memberType;
-	}
-
-	public int getId() {
-		return id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	
-	public MemberType getMemberType() {
-		return memberType;
-	}
-
-	public boolean isActive() {
-		return active;
-	}
-
 	public enum MemberType{
 		Silver,Gold,Diamond,Platinum
 	}
