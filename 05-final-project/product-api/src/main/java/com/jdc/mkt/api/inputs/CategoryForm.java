f0d105b1a7b166e25c5109045d95759820564192
uthuyein <uthuyeinlinn@gmail.com>
2026-01-05 15:51:22 +0630

    update validate message

--- 05-final-project/product-api/src/main/java/com/jdc/mkt/api/inputs/CategoryForm.java
+++ 05-final-project/product-api/src/main/java/com/jdc/mkt/api/inputs/CategoryForm.java
@@ -3,20 +3,25 @@ package com.jdc.mkt.api.inputs;
 import com.jdc.mkt.model.entities.Category;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CategoryForm(
 		@NotBlank(message = "Please type category name !") 
 		String name,
-		Integer categoryId) {
+		@NotNull(message = "Please type category id !")
+		Integer categoryId,
+		Boolean isActive) {
 
 	public Category entity() {
 		var cat = new Category();
 		cat.setName(name);
+		cat.setActive(isActive);
 		
 		var catId = new Category();
 		
 		if (null != categoryId) {
 			catId.setId(categoryId);
+			
 			cat.setCategory(catId);
 		}
 		return cat;
---
+++ 05-final-project/product-api/src/main/java/com/jdc/mkt/utils/ExceptionHandlers.java
@@ -0,0 +1,21 @@
+package com.jdc.mkt.utils;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlers {
+
+	@ExceptionHandler
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	List<String> handle(MethodArgumentNotValidException e){
+		return e.getFieldErrors()
+				.stream()
+				.map(error -> error.getDefaultMessage()).toList();
+	}
+}
